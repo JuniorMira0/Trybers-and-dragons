27c8f1305ab0005e583142976b8417659cdd2db2
junior <j-unyor14@hotmail.com>
2022-08-15 16:02:56 -0300

    create class Elf

---
+++ src/Races/Elf.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+export default class Elf extends Race {
+  private _maxLifePoints = 99;
+  private static _counter = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    Elf._counter += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Elf._counter;
+  }
+}
